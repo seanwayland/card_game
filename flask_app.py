8f891c0c795059feaa02034858162c47e3cf1365
seanwayland <seanwayland@gmail.com>
2020-10-26 21:31:32 -0400

    Create flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,13 @@
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello Seans from Flask!'
+
+@app.route('/page2')
+def page_two():
+    return 'Hello Seans from Flask page 2!'
